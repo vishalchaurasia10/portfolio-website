97a53fd3fc2020136d6a7f358aea72917bba4141
vishalchaurasia10 <1ms21cs138@msrit.edu>
2023-08-05 03:51:33 +0530

    made the intro section

---
+++ src/components/Intro.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect, useRef } from 'react'
+import { useGlitch } from 'react-powerglitch'
+import Lottie from 'lottie-react'
+import walkingPerson from '@/assets/animations/walkingPerson'
+import scrollDown from '@/assets/animations/scrollDown'
+import { motion } from 'framer-motion'
+import Typed from 'typed.js';
+
+const Intro = () => {
+    const glitch = useGlitch();
+    const el = useRef(null);
+    const el2 = useRef(null);
+
+    useEffect(() => {
+        if (el.current == null) return;
+        const typed = new Typed(el.current, {
+          strings: ['Hi,I am'],
+          typeSpeed: 30,
+        });
+    
+        if (el2.current == null) return;
+        const typed2 = new Typed(el2.current, {
+          strings: ['19 y/o Full-Stack Developer from India.'],
+          typeSpeed: 30,
+        });
+    
+        return () => {
+          // Destroy Typed instance during cleanup to stop animation
+          typed.destroy();
+          typed2.destroy();
+        };
+      }, []);
+
+    return (
+        <>
+            <div className="intro bg-black h-screen flex items-center justify-center overflow-hidden">
+                {/* <div className='h-1/2 w-1/4 bg-black animate-moveX -translate-x-80'>
+                    <Lottie animationData={walkingPerson} style={{ width: '100%', height: '100%' }} />
+                </div> */}
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ delay: 0, duration: 1 }}
+                    className="introText flex flex-col relative px-2 md:px-0 -top-10 items-center justify-center space-y-8">
+                    <h1 ref={el} className="text-2xl md:text-4xl text-[#58ff13] lg:px-40 font-firaCode text-center font-bold typed-text"></h1>
+                    <p ref={glitch.ref} className='text-4xl lg:text-8xl font-extrabold text-[#58ff13] font-firaCode'>Vishal Chaurasia</p>
+                    <p ref={el2} className="text-2xl md:text-4xl text-[#58ff13] lg:px-40 font-firaCode text-center font-bold typed-text" ></p>
+                </motion.div>
+                <div className='h-10 bg-black absolute bottom-20 cursor-pointer'>
+                    <Lottie animationData={scrollDown} />
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Intro
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,28 @@ module.exports = {
   ],
   theme: {
     extend: {
+
+      keyframes: {
+        'moveX':{
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(400%)' },
+        }
+      },
+
+      animation: {
+        'moveX': 'moveX 12s 1',
+      },
+
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+
+      fontFamily: {
+        'firaCode' : ['Fira Code', 'monospace'],
+        'pixel' : ['VT323', 'monospace'],
+        'pressStart' : ['Press Start 2P', 'cursive'],
       },
     },
   },
